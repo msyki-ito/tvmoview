835481dc6ef9028e7a02e03de331e8b42003d5e7
msyki-ito <enterintoo@gmail.com>
2025-07-02 23:25:30 +0900

    Fix missing border import

--- app/src/main/java/com/example/tvmoview/presentation/components/ModernMediaCard.kt
+++ app/src/main/java/com/example/tvmoview/presentation/components/ModernMediaCard.kt
@@ -3,6 +3,7 @@
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
 import androidx.compose.ui.focus.onFocusChanged
