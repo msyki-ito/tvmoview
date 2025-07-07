104dcc100291f47dbf2fae7ccfa10bff62d9feda
msyki-ito <enterintoo@gmail.com>
2025-07-07 11:44:35 +0900

    Fix missing Modifier import

--- app/src/main/java/com/example/tvmoview/presentation/theme/TVMovieTheme.kt
+++ app/src/main/java/com/example/tvmoview/presentation/theme/TVMovieTheme.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 
 // ダークテーマのカラーパレット
